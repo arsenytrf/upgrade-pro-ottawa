4151c099899aca39fab3e29742f31c5b2739845c
Arseni Filon <66354981+arsenytrf@users.noreply.github.com>
2026-04-17 17:53:18 -0700

    deploy

---
+++ _next/static/iCedPPFCSBEWr6iBmQe9M/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/upgrade-pro-ottawa//_next/:path+",
+        "destination": "/upgrade-pro-ottawa/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
